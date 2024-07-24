77987f2fa2b760ba9c2d7da5a1615f2c6daf85f1
wasil914 <144197727+wasil914@users.noreply.github.com>
2024-07-25 00:36:07 +0530

    script.js

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ function showSubject() {
         document.getElementById(selectedSubject).style.display = 'block';
     }
 }
+
+function playVideo() {
+    const videoContainer = document.getElementById('videoContainer');
+    videoContainer.innerHTML = `
+        <iframe title="vimeo-player" src="https://player.vimeo.com/video/989727117?h=69a9d2ca4a" width="640" height="360" frameborder="0" allowfullscreen></iframe>
+    `;
+}
